2d92867490300e863e1e8ca81d1a41aaa06281d7
notu <notuart@pm.me>
2023-07-18 00:57:10 +0100

    Troubleshooting typescript and tests

---
+++ packages/cli/src/cli.test.ts
@@ -0,0 +1,10 @@
+import { exec } from "child_process";
+
+describe("Check console.log output", () => {
+  test("outputs the correct value", (done) => {
+    exec("node ../dist/cli.js", (error, stdout) => {
+      expect(stdout).toBe("Hello, World!\n");
+      done();
+    });
+  });
+});
