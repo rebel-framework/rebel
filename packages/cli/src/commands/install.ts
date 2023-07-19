d1e5389bf4e902664bec6c5c7559c12a0bd0a25a
notu <notuart@pm.me>
2023-07-19 21:51:10 +0100

    [@rebel/cli] Cleanup basic install command implementation

--- packages/cli/src/commands/install.ts
+++ packages/cli/src/commands/install.ts
@@ -3,22 +3,15 @@ import { join } from "path";
 import { promises as fs } from "fs";
 
 export const install: Command = async (args: string[]) => {
-  // Parse the args
-  const packageName = args[0];
-
   // Create a .rebel directory if it doesn't already exist
   const currentDir = process.cwd();
   const rebelDir = join(currentDir, ".rebel");
 
   try {
     await fs.access(rebelDir);
-    console.log(".rebel directory already exists.");
+    console.log("Rebel is already installed.");
   } catch (error) {
     await fs.mkdir(rebelDir);
-    console.log(".rebel directory created.");
+    console.log("Rebel was successfully installed.");
   }
-
-  // Perform the installation
-  // This is just a placeholder, replace with actual implementation
-  console.log(`Installing package ${packageName}`);
 };
