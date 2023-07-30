529a0f682e8d78b93531dd57e6067e686bc586ee
notu <notuart@pm.me>
2023-07-30 12:55:48 +0100

    Simplify init command

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,43 +1,19 @@
 import { Command } from '@rebel/core';
-import { promises as fs } from 'fs';
 import { exec } from 'child_process';
 
-// OPPORTUNITY: Move this function to @rebel/core
-async function isDirectoryEmpty(dirPath: string): Promise<boolean> {
-  try {
-    const files = await fs.readdir(dirPath);
-    return files.length === 0;
-  } catch (error) {
-    // Handle error (directory doesn't exist, or there was a problem reading it)
-    return false;
-  }
-}
-
 const init: Command = async (args: string[]) => {
-  const currentDir = process.cwd();
-  const isEmpty = await isDirectoryEmpty(currentDir);
-  const [name] = args;
+  const [directory] = args;
 
-  if (!name) {
-    return console.warn(`Missing argument 'name'`);
+  const commands = [
+    `mkdir ${directory}`,
+    `npx degit https://github.com/rebeljs/skeleton.git ${directory} --force`,
+    `git init ${directory}`,
+    `mkdir ${directory}/.rebel`,
+  ];
+
+  for (let command of commands) {
+    await exec(command);
   }
-
-  await exec(`mkdir ${name}`);
-
-  if (!isEmpty) {
-    return console.warn(
-      'Cannot install Rebel because the current directory is not empty.'
-    );
-  }
-
-  try {
-    await exec(`npx degit https://github.com/rebeljs/skeleton.git ${name}`);
-  } catch (error) {
-    console.error('Error cloning repository:', error.message);
-  }
-
-  // TODO: git init
-  // TODO: run npm install
 
   console.log('Rebel was successfully installed.');
 };
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,17 +2,21 @@
 
 import * as commands from './commands';
 
-const args = process.argv.slice(2);
-const command = args[0];
+const main = async () => {
+  const args = process.argv.slice(2);
+  const command = args[0];
 
-// Remove the command name from the arguments
-args.shift();
+  // Remove the command name from the arguments
+  args.shift();
 
-if (!commands[command]) {
-  console.log(`Unknown command: ${command}`);
-  console.log(
-    'Available commands: init, install, build, test, deploy, generate'
-  );
-} else {
-  commands[command](args);
-}
+  if (!commands[command]) {
+    console.log(`Unknown command: ${command}`);
+    console.log(
+      'Available commands: init, install, build, test, deploy, generate'
+    );
+  } else {
+    await commands[command](args);
+  }
+};
+
+main();
