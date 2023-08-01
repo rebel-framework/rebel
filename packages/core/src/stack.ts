530e80136feb95919a5be5ca052b6c193e7a2dd3
notu <notuart@pm.me>
2023-08-02 00:06:08 +0100

    Update stack.ts

--- packages/core/src/stack.ts
+++ packages/core/src/stack.ts
@@ -3,11 +3,15 @@ import * as Lambda from 'aws-cdk-lib/aws-lambda';
 import * as ApiGateway from 'aws-cdk-lib/aws-apigateway';
 import * as DynamoDB from 'aws-cdk-lib/aws-dynamodb';
 import * as S3 from 'aws-cdk-lib/aws-s3';
-import * as S3Deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as S3Deployment from 'aws-cdk-lib/aws-s3-deployment';
 import * as CloudFront from 'aws-cdk-lib/aws-cloudfront';
 import * as SNS from 'aws-cdk-lib/aws-sns';
 import * as Subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as SQS from 'aws-cdk-lib/aws-sqs';
+import * as IAM from 'aws-cdk-lib/aws-iam';
+import * as Logs from 'aws-cdk-lib/aws-logs';
+import * as Route53 from 'aws-cdk-lib/aws-route53';
+
 import { Construct } from 'constructs';
 
 // const app = () => {
@@ -37,9 +41,83 @@ export const useStack = (stackName: string, stackProps?: StackProps) => {
   const subscription = (topic: SNS.Topic, queue: SQS.Queue) =>
     topic.addSubscription(new Subs.SqsSubscription(queue));
 
+  const role = (roleName: string, roleProps: IAM.RoleProps) =>
+    new IAM.Role(stack, roleName, roleProps);
+
+  const logGroup = (logGroupName: string, logGroupProps: Logs.LogGroupProps) =>
+    new Logs.LogGroup(stack, logGroupName, logGroupProps);
+
+  const table = (tableName: string, tableProps: DynamoDB.TableProps) =>
+    new DynamoDB.Table(stack, tableName, tableProps);
+
+  const bucket = (bucketName: string, bucketProps: S3.BucketProps) =>
+    new S3.Bucket(stack, bucketName, bucketProps);
+
+  const bucketDeployment = (
+    deploymentId: string,
+    source: S3Deployment.ISource,
+    bucket: S3.IBucket,
+    deploymentProps?: S3Deployment.BucketDeploymentProps
+  ) =>
+    new S3Deployment.BucketDeployment(stack, deploymentId, {
+      sources: [source],
+      destinationBucket: bucket,
+      ...deploymentProps,
+    });
+
+  const apiGateway = (apiName: string, apiProps: ApiGateway.RestApiProps) =>
+    new ApiGateway.RestApi(stack, apiName, apiProps);
+
+  const resource = (
+    api: ApiGateway.RestApi,
+    pathPart: string,
+    parent?: ApiGateway.IResource
+  ) => (parent ? parent.addResource(pathPart) : api.root.addResource(pathPart));
+
+  const method = (
+    resource: ApiGateway.IResource,
+    httpMethod: string,
+    integration: ApiGateway.Integration,
+    methodOptions?: ApiGateway.MethodOptions
+  ) => resource.addMethod(httpMethod, integration, methodOptions);
+
+  const originAccessIdentity = () =>
+    new CloudFront.OriginAccessIdentity(stack, 'MyOriginAccessIdentity');
+
+  const cloudFrontWebDistribution = (
+    webDistributionProps: CloudFront.CloudFrontWebDistributionProps
+  ) =>
+    new CloudFront.CloudFrontWebDistribution(
+      stack,
+      'MyCDN',
+      webDistributionProps
+    );
+
+  const hostedZone = (
+    zoneName: string,
+    hostedZoneProps?: Route53.HostedZoneProps
+  ) => new Route53.HostedZone(stack, zoneName, hostedZoneProps);
+
   const deploy = () => {
     // app.synth();
   };
 
-  return { deploy, queue, topic, lambda, subscription };
+  return {
+    deploy,
+    queue,
+    topic,
+    lambda,
+    subscription,
+    role,
+    logGroup,
+    table,
+    bucket,
+    bucketDeployment,
+    apiGateway,
+    resource,
+    method,
+    originAccessIdentity,
+    cloudFrontWebDistribution,
+    hostedZone,
+  };
 };
