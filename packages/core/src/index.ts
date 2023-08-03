007692900bb7214caf628ed016e6bb9d0f405031
notu <notuart@pm.me>
2023-08-04 00:12:01 +0100

    Update index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,7 @@ export type { Command, Manifest, Middleware, Route } from './types';
 export { HttpMethod } from './enums';
 export { useDatabase } from './database';
 export { useDeployer } from './deployer';
+export { useManifest } from './manifest';
 export { useRouter } from './router';
 export { useStack } from './stack';
 export { env } from './helpers/env';
