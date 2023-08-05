f794de2c170b0be6cbe1d2d886a518e05bed68c7
notu <notuart@pm.me>
2023-08-06 00:11:08 +0100

    Upgrade cdk

--- packages/cli/src/commands/deploy.ts
+++ packages/cli/src/commands/deploy.ts
@@ -2,12 +2,12 @@ import { root } from '@rebel/core';
 import { spawn } from 'child_process';
 import path from 'path';
 
-export default async function deploy<Command>(args: string[]) {
+export default async function deploy(args: string[]) {
   // TODO:
   // - Get current aws-cdk-lib version from package.json
   // - Use that version in command
 
-  const cdk = 'aws-cdk@^2.89.0';
+  const cdk = 'aws-cdk@^2.90.0';
   const app = path.resolve(`${__dirname}/deploy/stack.js`);
   const output = root('.rebel/cdk');
 
