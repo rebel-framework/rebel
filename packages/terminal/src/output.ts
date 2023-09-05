5c63322a839c5c7a0a19c6282cd0e9cba318ed14
notu <notuart@pm.me>
2023-09-05 22:11:45 +0100

    Terminal: remove 'program' function

--- packages/terminal/src/output.ts
+++ packages/terminal/src/output.ts
@@ -7,16 +7,10 @@ type TerminalOutput = Writable & {
   setRawMode: (value: boolean) => void;
 };
 
-export function terminal(): NodeJS.Process {
-  return process;
-}
-
-export function stdout() {
-  return process.stdout as Writable;
-}
+const stdout = process.stdout as Writable;
 
 export function setRawMode(value: boolean) {
-  const tty = stdout() as TerminalOutput;
+  const tty = stdout as TerminalOutput;
   if (tty && tty.isTTY && tty.setRawMode) {
     tty.setRawMode(value);
   }
@@ -31,7 +25,7 @@ export function resetCursor() {
 }
 
 export function write(message: string) {
-  stdout().write(message);
+  stdout.write(message);
 }
 
 export function lineBreak() {
