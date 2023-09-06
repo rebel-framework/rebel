2ead872ef3911d961a1efa20c2bb8ceecaf5edba
notu <notuart@pm.me>
2023-09-06 23:15:12 +0100

    Fixing terminal package test runer

--- packages/terminal/test/input.test.ts
+++ packages/terminal/test/input.test.ts
@@ -23,6 +23,8 @@ describe('input.ts', () => {
   let mockSetEncoding: jest.SpyInstance;
 
   beforeEach(() => {
+    jest.spyOn(process.stdin, 'setRawMode').mockImplementation();
+
     mockOnce = jest.spyOn(process.stdin, 'once').mockImplementation();
     mockOn = jest.spyOn(process.stdin, 'on').mockImplementation();
     mockRemoveListener = jest
