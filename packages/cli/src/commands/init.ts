157590c4658a361c6deb55eecbffd14bc65787e1
notu <notuart@pm.me>
2023-09-06 23:10:10 +0100

    Improving rebel init command

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,13 +1,14 @@
 import { exec } from 'child_process';
+import { join } from 'path';
 
 export default async function init(args: string[]) {
   const [directory] = args;
 
   const commands = [
-    `mkdir ${directory}`,
-    `npx degit https://github.com/rebel-framework/skeleton.git ${directory} --force`,
-    `git init ${directory}`,
-    `mkdir ${directory}/.rebel`,
+    `mkdir "${directory}"`,
+    `npx degit https://github.com/rebel-framework/skeleton.git "${directory}" --force`,
+    `git init "${directory}"`,
+    `mkdir "${join(directory, '.rebel')}"`,
   ];
 
   for (let command of commands) {
