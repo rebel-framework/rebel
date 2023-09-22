26db896823e3ff61a0176497c22b5b65ffbdfe1d
notu <notuart@pm.me>
2023-09-22 13:55:15 +0100

    Refactor init command

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -1,7 +1,6 @@
 export { default as build } from './build';
-export { default as create } from './create';
 export { default as deploy } from './deploy';
 export { default as destroy } from './destroy';
-export { default as init } from './init';
 export { default as hello } from './hello';
+export { default as init } from './init';
 export { default as run } from './run';
--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,16 +1,8 @@
-import {
-  ask,
-  choice,
-  confirm,
-  line,
-  success,
-  fail,
-  bold,
-} from '@rebel-framework/terminal';
+import { success, fail } from '@rebel-framework/terminal';
 import { exec } from 'child_process';
 import { join } from 'path';
-import readPackageJson from '../helpers/read-package-json';
 import { Signature } from '../types';
+import isDirectoryEmpty from '../helpers/is-directory-empty';
 
 export const signature: Signature = {
   options: {
@@ -23,61 +15,27 @@ export const signature: Signature = {
   },
 };
 
-const state = {
-  existingProject: true,
-  monorepo: false,
-  packageData: undefined,
-};
-
 async function init({ directory }) {
   directory = directory || signature.options.directory.default;
 
-  try {
-    state.packageData = readPackageJson(directory);
-  } catch (error) {
-    state.existingProject = false;
+  const empty = await isDirectoryEmpty(directory, true);
+
+  if (!empty) {
+    fail('Directory is not empty');
   }
 
-  if (state.packageData) {
-    state.monorepo = state.packageData.workspaces !== undefined;
+  const commands = [
+    `mkdir "${directory}"`,
+    `npx degit https://github.com/rebel-framework/skeleton.git "${directory}" --force`,
+    `git init "${directory}"`,
+    `mkdir "${join(directory, '.rebel')}"`,
+  ];
+
+  for (let command of commands) {
+    await exec(command);
   }
 
-  if (
-    state.existingProject &&
-    (await confirm(
-      'This is an existing project and changes will be made to the codebase. Are you sure?',
-      false
-    ))
-  ) {
-    console.log('Existing project and OK');
-    if (state.packageData && state.packageData.workspaces) {
-      const location = await ask(
-        'Should we create a stack for each workspace?',
-        state.packageData.workspaces
-      );
-
-      console.log({ location });
-    }
-  } else {
-    console.log('New project');
-  }
-
-  console.log({ state });
-
-  process.exit();
-
-  // const commands = [
-  //   `mkdir "${directory}"`,
-  //   `npx degit https://github.com/rebel-framework/skeleton.git "${directory}" --force`,
-  //   `git init "${directory}"`,
-  //   `mkdir "${join(directory, '.rebel')}"`,
-  // ];
-
-  // for (let command of commands) {
-  //   await exec(command);
-  // }
-
-  // console.log('Rebel was successfully installed.');
+  success('Rebel was successfully installed.');
 }
 
 export default {
