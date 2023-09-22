95f4be40cd347a8c15851f2e9af6d23b0b25a5b1
notu <notuart@pm.me>
2023-09-22 14:48:01 +0100

    Remove unused cli commands

--- packages/cli/src/commands/build.ts
+++
@@ -1,5 +0,0 @@
-export default async function build(args: string[]) {
-  console.log('Build project');
-
-  // Take entry point, build handler with all routes and middleware
-}
--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -1,6 +1,3 @@
-export { default as build } from './build';
 export { default as deploy } from './deploy';
 export { default as destroy } from './destroy';
-export { default as hello } from './hello';
 export { default as init } from './init';
-export { default as run } from './run';
